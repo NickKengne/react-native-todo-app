62cd6847ea53a2a09d96ebaa8cdf90e86da0f097
nickdk294 <nickdk294@gmail.com>
2023-04-20 20:26:23 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+export default function App() {
+  return (
+    <SafeAreaView>
+        
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
